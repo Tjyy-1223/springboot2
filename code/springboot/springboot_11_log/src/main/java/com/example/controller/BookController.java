eab6125dc3ca023843def8624f2708e9c2ce99ad
tianjiangyu <161840237@nuaa.edu.cn>
2022-07-11 13:37:55 +0800

    lpg output

--- code/springboot/springboot_11_log/src/main/java/com/example/controller/BookController.java
+++ code/springboot/springboot_11_log/src/main/java/com/example/controller/BookController.java
@@ -1,16 +1,31 @@
 package com.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    // 创建记录日志的对象
+//    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     @GetMapping
     public String getById(){
         System.out.println("spring boot  is running");
+        log.debug("debug ...");
+        log.info("info ...");
+        log.warn("warn ...");
+        log.error("error ...");
+
         return "spring boot is  running";
     }
+
+
 
 }
